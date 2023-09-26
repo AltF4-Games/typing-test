34b91e554a4dff15a95e15345ce7a75e6346f4cc
FreezeStudios <39883432+AltF4-Games@users.noreply.github.com>
2023-09-26 19:54:48 +0530

    Grand theft

--- typing-test.js
+++ typing-test.js
@@ -1,18 +1,20 @@
-let str = "the quick brown fox jumps over the lazy dog exciting adventure awaits in the enchanted forest beautiful flowers bloom in vibrant colors majestic mountains stand tall against the azure sky serene lakes shimmer under the gentle moonlight a melodious song fills the air as birds sing harmoniously brilliant fireworks light up the night sky whispering winds carry secrets through the ancient trees busy streets bustle with life as people rush to their destinations delicious aromas waft from the bustling cafes silky sand tickles toes on sunny beaches quaint cottages line the cobblestone streets charming shops display unique treasures that beckon to be discovered vibrant markets bustle with colors and scents ancient ruins whisper tales of the past glimmering stars adorn the night sky as a crescent moon watches over the world inspiring books transport minds to different realms with their vivid stories sparkling fountains dance in the sunlight while children laugh with joy tranquil temples offer solace and peace to weary souls endless possibilities await those who embrace the unknown timeless wisdom guides us on our journey kind-hearted friends lend a helping hand during difficult times juicy strawberries tempt taste buds with their sweetness captivating artworks evoke emotions and ignite imagination rainy days create a cozy atmosphere for curling up with a good book thunderous applause fills the concert hall as the orchestra performs a breathtaking symphony the scent of freshly brewed coffee awakens the senses on a crisp morning breeze wise elders share their stories around the crackling fireplace twinkling fairy lights create a magical ambiance in the garden soothing raindrops create a rhythmic melody on the roof a gentle breeze rustles through the autumn leaves as they fall to the ground delightful laughter echoes through the playground as children play games of tag and hide-and-seek intricate lace patterns adorn the wedding dress of the blushing bride colorful kites soar high in the sky during a windy day a lively street parade";
-let words = str.split(' ');
+let str = "";
 
-shuffleArray(words);
-words = words.splice(50,50);
-str = words.join(' ');
+fetch('./data.json')
+    .then((response) => response.json())
+    .then((json) => { 
+        str = json.quotes[Math.floor(Math.random() * json.quotes.length)].text.toLowerCase();
+        AddElement();
+    });
+
 
 let wordsContainer = document.getElementById("words");
 let correctCharacters = [words.length];
 
 let spans = [];
-AddElement()
 
 let input = "";
-let validKeys = "abcdefghijklmnopqrstuvwxyz ";
+let validKeys = "abcdefghijklmnopqrstuvwxyz!?,.':; ";
 
 let dispalyTime = 30;
 let firstKey = false;
@@ -22,13 +24,6 @@ let time = 0;
 window.addEventListener('keydown', function (e) {
     HandleInput(e.key);
 }, false);
-
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-}
 
 const button = document.getElementById("retry-btn");
 
@@ -142,7 +137,7 @@ function CalculateWPM()
 
     for (let index = 0; index < totalWords; index++) 
     {
-        if(inputArray[index] == words[index])
+        if (inputArray[index] == str.split(' ')[index])
         {
             wordCount++;
         }
@@ -160,6 +155,6 @@ function CalculateWPM()
         }
     });
 
-    let accuracy = Math.trunc(accurate/input.length * 100);
+    let accuracy = Math.trunc((accurate / str.length) * 100);
     document.getElementById("result").innerHTML = "WPM: " + wpm + " ACCURACY: " + accuracy + "%";
 }
